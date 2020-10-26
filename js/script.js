b0ebfa24e12e6d2275162f2f8639c7004cdce2ba
JRJ <jjsocc171@msn.com>
2020-10-26 11:50:46 -0500

    timer stops at 0

--- js/script.js
+++ js/script.js
@@ -1,31 +1,93 @@
 var timerEl = document.getElementById('countdown');
 var mainEl = document.getElementById('main');
-var startBtn = document.getElementById('start');
-
-var timeLimit = 60;
-timerEl.textContent = timeLimit
+var startBtnEl = document.getElementById('start-quiz');
+var welcomePageEl = document.getElementById('welcome-page');
+var questionEl = document.getElementById('ul')
+var questionListEl = document.querySelector("question-list");
+var answerListEl = document.querySelector('#answers')
+var score = 0;
+var currentQuestion = 0
+var timeLimit = 10;
+timerEl.textContent = timeLimit;
 var timer;
+//var time = 0;
+var penalty = 10;
+
+
+
 
 var questions = [
-    {q: 'what color is the sky:', a: 'blue' },
-    {q: 'An array is used for:', a: 'store multiple values in a single variable'},
-    {q: ' ', a: ' '},
-    {q: ' ', a: ' '},
-    {q: ' ', a: ' '},
-    {q: ' ', a: ' '},
+    { q: 'Arrays in JavaScript can be used to store _____.',
+    a:  ["Numbers & strings", "Other arrays", "Booleans", "All of the above"],
+    correctAnswer: "all of the above"},
+
+    { q: 'The condition in an if/else statement is enclosed with ______.',
+    a: ["Quotes", "Curly brackets", "Parenthesis", "Square brackets"],
+    correctAnswer: "Curly brackets",
+    },
+
+    {q: 'String values must be closed within ____ when being assigned to variables.', 
+    a: ["Commas", "Curly Brackets", "Quotes", "Parentheses"],
+    correctAnswer: "Quotes"
+    },
+
+    {q: 'A very useful tool used during development and debugging for printing content to the debugger is:',
+    a: ["JavaScript", "Terminal/bash", "For loops", "Console.log"],
+    correctAnswer: "Console.log"
+    },
+
+    {q: 'Commonly used data types DO Not include:', 
+    a: ["Strings", "Booleans", "Alerts", "Numbers"],
+    correctAnswer: "Alerts"
+    },
+
+    {q: 'What does DOM stand for:', 
+    a: ["Document of Mobile", "Document Object Model", "Dominant Object Model", "Domain of Model"],
+    correctAnswer: "Document Object Model"
+    },
 ];
 
-//start game at 0
-var score = 0;
 
-//for loop questions
-for (var i = 0; i < questions.length; i++) {
+// start timer when start quiz button is clicked
+startBtnEl.addEventListener("click", function() {
+    //console.log("test")
+        timer = setInterval(function() {
+            timerEl.textContent = timeLimit--;
+            timerEl.textContent = "Time:" + timeLimit;
+
+        if (timeLimit > 0) {
+            timeLimit--;
+        } else {
+            clearInterval(timer);
+            timerEl.textContent = "Time's Up!";
+            allDone();
+        }  
+    },1000);   
+    startQuiz();    
+});
+
+function startQuiz() {
+    welcomePageEl.remove();
+    // startQuestions();
+    for (var i = 0; i < questions[currentQuestion].a.length; i++) {
+        var quizQuestions = questions[i].q;
+        var quizAnswers = questions[i].a;
+        questionEl.textContent = quizQuestions;
+    }
+    // quizAnswers.forEach(function (answerItem) {
+    //     var answerList = document.createElement("li");
+    //     answerList.textContent = answerItem;
+    //     questionListEl.appendChild(welcomePageEl);
+    //     welcomePageEl.appendChild(answerList);
+    //     answerList.addEventListener("click");
+    //}
 
 };
 
-startBtn.addEventListener("click", function(){
-    console.log("test")
-    timer = setInterval(function(){
-        timerEl.textContent = timeLimit--;
-    },1000)
-})
+function stopTime() {
+    clearInterval(timer);
+};
+
+
+
+startBtnEl.addEventListener("click", startQuiz);
