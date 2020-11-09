d9a37a5fbe8da9394578a3c9cab83a4e9b009eee
JRJ <jjsocc171@msn.com>
2020-11-08 21:08:16 -0600

    localStorage added

--- js/script.js
+++ js/script.js
@@ -1,18 +1,18 @@
 
 // Questions & Answers
 var questions = [
-    { 
+    {
         questionTitle: "Arrays in JavaScript can be used to store _____.",
-        answerChoice:  ["Numbers & strings", "Other arrays", "Booleans", "All of the above"],
+        answerChoice: ["Numbers & strings", "Other arrays", "Booleans", "All of the above"],
         correctAnswer: "All of the above"
     },
-    { 
+    {
         questionTitle: "The condition in an if/else statement is enclosed with ______.",
         answerChoice: ["Quotes", "Curly brackets", "Parenthesis", "Square brackets"],
         correctAnswer: "Curly brackets",
     },
     {
-        questionTitle: "String values must be closed within ____ when being assigned to variables.", 
+        questionTitle: "String values must be closed within ____ when being assigned to variables.",
         answerChoice: ["Commas", "Curly Brackets", "Quotes", "Parentheses"],
         correctAnswer: "Quotes"
     },
@@ -22,12 +22,12 @@ var questions = [
         correctAnswer: "Console.log"
     },
     {
-        questionTitle: "Commonly used data types DO Not include:", 
+        questionTitle: "Commonly used data types DO Not include:",
         answerChoice: ["Strings", "Booleans", "Alerts", "Numbers"],
         correctAnswer: "Alerts"
     },
     {
-        questionTitle: "What does DOM stand for:", 
+        questionTitle: "What does DOM stand for:",
         answerChoice: ["Document of Mobile", "Document Object Model", "Dominant Object Model", "Domain of Model"],
         correctAnswer: "Document Object Model"
     },
@@ -55,21 +55,21 @@ var correctAnswer = 0
 
 
 function startQuiz() {
-    timer = setInterval(function() {
-        timerEl.textContent = timeLimit--;             
+    timer = setInterval(function () {
+        timerEl.textContent = timeLimit--;
         timerEl.textContent = "Time:" + timeLimit;
-    
-    if (timeLimit > 0) {
-        timeLimit--;
-    } else {
-        clearInterval(timer);
-        endGame();
-        timerEl.textContent = "Time's Up!";
-    }  
-    },1000);  
+
+        if (timeLimit > 0) {
+            timeLimit--;
+        } else {
+            clearInterval(timer);
+            endGame();
+            timerEl.textContent = "Time's Up!";
+        }
+    }, 1000);
 
     startQuestions();
-}     
+}
 
 function startQuestions() {
     containerEl.innerHTML = "";
@@ -96,12 +96,12 @@ function startQuestions() {
     // buttonHandler();
 }
 
-var buttonHandler = function(event) {
+var buttonHandler = function (event) {
     // console.log(event.target.value);
     // var element = event.target;
-    console.log(event.target.matches(".btn-choices-button"));
+    // console.log(event.target.matches(".btn-choices-button"));
     if (event.target.matches(".btn-choice-button")) {
-         var elementId = event.target.value;
+        var elementId = event.target.value;
         // console.log(elementId, "value");
 
         if (elementId === questions[questionIndex].correctAnswer) {
@@ -126,14 +126,15 @@ var buttonHandler = function(event) {
             questionIndex++;
             startQuestions();
         }
-    }  
+    }
 };
 
-var endGame = function() {
-    clearInterval(timeLimit);
+var endGame = function () {
+    // clearInterval(timeLimit);
 
     if (questionIndex >= questions.length) {
         containerEl.textContent = "End of Quiz!"
+        alert("final score is: " + timeRemaining)
     } else {
         render(questionIndex);
     }
@@ -144,36 +145,50 @@ var endGame = function() {
     if (timeLimit >= 0) {
         var timeRemaining = timeLimit;
         var createP = document.createElement("p");
+        var input = document.createElement("input");
+        var submitButton = document.createElement("button");
+        input.classList.add("initials-input");
+        submitButton.classList.add("submitButton");
+        // var submitButton = document.querySelector(".submitButton")
+        submitButton.addEventListener('click', function () {
+            var inputEl = document.querySelector(".initials-input");
+            var initials = inputEl.value;
+             console.log(initials);
+        setScore();
+        })
+
         clearInterval(timer);
         createP.textContent = "Your final score is: " + timeRemaining;
+        submitButton.textContent = "Submit";
         containerEl.appendChild(createP);
+        containerEl.appendChild(input);
+        containerEl.appendChild(submitButton);
     }
 
     var createDivHighscore = document.createElement("div");
     createDivHighscore.setAttribute("id", "createDivHighscore");
     containerEl.appendChild(createDivHighscore);
 
-    var setScore = function() {
-        localStorage.setItem("highscore", timeRemaining);
-        console.log("highscore saved");
+    var setScore = function () {
+        localStorage.setItem("initials", timeRemaining);
+        console.log("initials saved");
     }
 };
 
 
-        // createDiv.setAttribute("taskId", "createDiv");
+// createDiv.setAttribute("taskId", "createDiv");
 
-        // if (targetEl.textContent == questions[questionIndex].correctAnswer) {
-        //     score++;
-        //     createDiv.textContent = "Correct! the answer is" + questions[questionIndex].correctAnswer;
+// if (targetEl.textContent == questions[questionIndex].correctAnswer) {
+//     score++;
+//     createDiv.textContent = "Correct! the answer is" + questions[questionIndex].correctAnswer;
 
-        // } else {
-        //     timeLimit = timeLimit - penalty;
-        //     createDiv.textContent = "Wrong! The correct answer is:" + questions[questionIndex].correctAnswer;
-        // }
+// } else {
+//     timeLimit = timeLimit - penalty;
+//     createDiv.textContent = "Wrong! The correct answer is:" + questions[questionIndex].correctAnswer;
+// }
 
 
 //         //taskId = targetEl.getAtt
-
 
 startBtnEl.addEventListener('click', startQuiz, startQuestions);
 containerEl.addEventListener('click', buttonHandler);   
